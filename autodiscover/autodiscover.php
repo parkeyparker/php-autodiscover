9437f3aca2ce7d58d20b2b5b76328ccf3e3f75f6
Aaron Parker <aaron@sourcecube.co.uk>
2020-03-29 15:30:38 +0100

    Added dot-env and updated htaccess

--- autodiscover/autodiscover.php
+++ autodiscover/autodiscover.php
@@ -1,5 +1,4 @@
 <?php
-
 /********************************
  * Autodiscover responder
  ********************************
@@ -12,84 +11,70 @@
  * To work properly, you'll need to set the service (sub)domains below in the settings section to the correct 
  * domain names, adjust ports and SSL.
  */
+require '../vendor/autoload.php';
 
-//get raw POST data so we can extract the email address
+// Get env vars
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// get raw POST data so we can extract the email address
 $request = file_get_contents("php://input");
 
 // optional debug log
-# file_put_contents( 'request.log', $request, FILE_APPEND );
+// file_put_contents( 'request.log', $request, FILE_APPEND );
 
 // retrieve email address from client request
-preg_match( "/\<EMailAddress\>(.*?)\<\/EMailAddress\>/", $request, $email );
+preg_match("/\<EMailAddress\>(.*?)\<\/EMailAddress\>/", $request, $email);
 
 // check for invalid mail, to prevent XSS
 if (filter_var($email[1], FILTER_VALIDATE_EMAIL) === false) {
-	throw new Exception('Invalid E-Mail provided');
+    throw new Exception('Invalid E-Mail provided');
 }
 
 // get domain from email address
-$domain = substr( strrchr( $request, "@" ), 1 );
+$domain = substr(strrchr($request, "@"), 1);
 
-/**************************************
- *   Port and server settings below   *
- **************************************/
-
-// POP settings
-$popServer = 'pop.' . $domain; // pop.example.com
-$popPort   = 110;
-$popSSL    = false;
-
-// IMAP settings
-$imapServer = 'imap.' . $domain; // imap.example.com
-$imapPort   = 993;
-$imapSSL    = true;
-
-// SMTP settings
-$smtpServer = 'smtp.' . $domain; // smtp.example.com
-$smtpPort   = 587;
-$smtpSSL    = true;
-
-//set Content-Type
-header( 'Content-Type: application/xml' );
+// set Content-Type
+header('Content-Type: application/xml');
 ?>
 <?php echo '<?xml version="1.0" encoding="utf-8" ?>'; ?>
-<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" >
-	<Response xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a">
-		<Account>
-			<AccountType>email</AccountType>
-			<Action>settings</Action>
-			<Protocol>
-				<Type>POP3</Type>
-				<Server><?php echo $popServer; ?></Server>
-				<Port><?php echo $popPort; ?></Port>
-				<LoginName><?php echo $email[1]; ?></LoginName>
-				<DomainRequired>off</DomainRequired>
-				<SPA>off</SPA>
-				<SSL><?php echo $popSSL ? 'on' : 'off'; ?></SSL>
-				<DomainRequired>off</DomainRequired>
-			</Protocol>
-			<Protocol>
-				<Type>IMAP</Type>
-				<Server><?php echo $imapServer; ?></Server>
-				<Port><?php echo $imapPort; ?></Port>
-				<DomainRequired>off</DomainRequired>
-				<LoginName><?php echo $email[1]; ?></LoginName>
-				<SPA>off</SPA>
-				<SSL><?php echo $imapSSL ? 'on' : 'off'; ?></SSL>
-				<AuthRequired>on</AuthRequired>
-			</Protocol>
-			<Protocol>
-				<Type>SMTP</Type>
-				<Server><?php echo $smtpServer; ?></Server>
-				<Port><?php echo $smtpPort; ?></Port>
-				<DomainRequired>off</DomainRequired>
-				<LoginName><?php echo $email[1]; ?></LoginName>
-				<SPA>off</SPA>
-				<SSL><?php echo $smtpSSL ? 'on' : 'off'; ?></SSL>
-				<AuthRequired>on</AuthRequired>
-				<UsePOPAuth>on</UsePOPAuth>
-				<SMTPLast>on</SMTPLast>
-			</Protocol>
-		</Account>
-	</Response>
+<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <Response xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a">
+    <Account>
+      <AccountType>email</AccountType>
+      <Action>settings</Action>
+      <Protocol>
+        <Type>POP3</Type>
+        <Server><?php echo getenv('POP_SERVER'); ?></Server>
+        <Port><?php echo getenv('POP_PORT'); ?></Port>
+        <LoginName><?php echo $email[1]; ?></LoginName>
+        <DomainRequired>off</DomainRequired>
+        <SPA>off</SPA>
+        <SSL><?php echo getenv('POP_SSL') ? 'on' : 'off'; ?></SSL>
+        <DomainRequired>off</DomainRequired>
+      </Protocol>
+      <Protocol>
+        <Type>IMAP</Type>
+        <Server><?php echo getenv('IMAP_SERVER'); ?></Server>
+        <Port><?php echo $getenv('IMAP_PORT'); ?></Port>
+        <DomainRequired>off</DomainRequired>
+        <LoginName><?php echo $email[1]; ?></LoginName>
+        <SPA>off</SPA>
+        <SSL><?php echo getenv('IMAP_SSL') ? 'on' : 'off'; ?></SSL>
+        <AuthRequired>on</AuthRequired>
+      </Protocol>
+      <Protocol>
+        <Type>SMTP</Type>
+        <Server><?php echo getenv('SMTP_SERVER'); ?></Server>
+        <Port><?php echo getenv('SMTP_PORT'); ?></Port>
+        <DomainRequired>off</DomainRequired>
+        <LoginName><?php echo $email[1]; ?></LoginName>
+        <SPA>off</SPA>
+        <SSL><?php echo getenv('SMTP_SSL') ? 'on' : 'off'; ?></SSL>
+        <AuthRequired>on</AuthRequired>
+        <UsePOPAuth>on</UsePOPAuth>
+        <SMTPLast>on</SMTPLast>
+      </Protocol>
+    </Account>
+  </Response>
 </Autodiscover>
